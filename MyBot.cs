341cef60e6e52450b5115b2eee6dbd68124a8193
domenSedlar <domen.sedlar@gmail.com>
2023-07-31 12:13:21 +0200

    changed the search algorithem to negamax

--- MyBot.cs
+++ MyBot.cs
@@ -2,21 +2,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks.Sources;
 
 public class MyBot : IChessBot
 {
+
     Dictionary<ulong, int> position_table = new Dictionary<ulong, int>();
     int[] pieceValues = { 60, 40, 27, 27, 10 };
 
     public Move Think(Board board, Timer timer)
     {
-
         int high_score = -1000;
         int move_i = 0;
         Move[] moves = board.GetLegalMoves();
         int c = moves.Length;
         int score = 0;
-        for (int i = 0; i<c;i++)
+        for (int i = 0; i < c; i++)
         {
 
             board.MakeMove(moves[i]);
@@ -33,20 +34,19 @@ public class MyBot : IChessBot
                 {
                     high_score = score;
                     move_i = i;
-                    Console.WriteLine(score);
-                    Console.WriteLine(moves[i].StartSquare.Name + " - " + moves[i].TargetSquare.Name);
+
                 }
                 board.UndoMove(moves[i]);
                 continue;
             }
 
-            score = RecursiveFunction(board, timer, 0);
+            score = NegaMax(board, timer, 2);
             position_table.Add(board.ZobristKey, score);
-            if (score > high_score){
+            if (score > high_score)
+            {
                 high_score = score;
                 move_i = i;
-                Console.WriteLine(score);
-                Console.WriteLine(moves[i].StartSquare.Name + " - " + moves[i].TargetSquare.Name);
+
 
             }
 
@@ -55,65 +55,59 @@ public class MyBot : IChessBot
         return moves[move_i];
     }
 
-    private int RecursiveFunction(Board board, Timer timer, int depth) {
-        int high_score = -1000;
-        int low_score = 1000;
+    private int Eval(Board board, int depth) {
+        Move[] moves = board.GetLegalMoves();
+        int p1 = moves.Length;
+        if (!board.TrySkipTurn())
+        {
+            return p1;
+        }
+
+        p1 = board.GetLegalMoves().Length + board.GetLegalMoves(true).Length
+                + board.GetPieceList(PieceType.Queen, board.IsWhiteToMove).Count * pieceValues[0]
+                + board.GetPieceList(PieceType.Rook, board.IsWhiteToMove).Count * pieceValues[1]
+                + board.GetPieceList(PieceType.Bishop, board.IsWhiteToMove).Count * pieceValues[2]
+                + board.GetPieceList(PieceType.Knight, board.IsWhiteToMove).Count * pieceValues[3]
+                + board.GetPieceList(PieceType.Pawn, board.IsWhiteToMove).Count * pieceValues[4];
+        board.UndoSkipTurn();
+
+        int p2 = board.GetLegalMoves().Length + board.GetLegalMoves(true).Length
+        + board.GetPieceList(PieceType.Queen, board.IsWhiteToMove).Count * pieceValues[0]
+        + board.GetPieceList(PieceType.Rook, board.IsWhiteToMove).Count * pieceValues[1]
+        + board.GetPieceList(PieceType.Bishop, board.IsWhiteToMove).Count * pieceValues[2]
+        + board.GetPieceList(PieceType.Knight, board.IsWhiteToMove).Count * pieceValues[3]
+        + board.GetPieceList(PieceType.Pawn, board.IsWhiteToMove).Count * pieceValues[4];
+
+        return (depth%2==0) ?  (p1 - p2) : -(p1-p2);
+    }
+
+    private int NegaMax(Board board, Timer timer, int depth)
+    {
+
+        if (depth == 0)
+        {
+            return Eval(board, depth);
+        }
+
+        int max_score = int.MinValue;
         int score = 0;
         Move[] moves = board.GetLegalMoves();
         int c = moves.Length;
-        int lm = 2;
-
-        if (depth == lm && lm%2 == 0)
-        {
-            if (!board.TrySkipTurn())
-            {
-                return c;
-            }
-            c = board.GetLegalMoves().Length + board.GetLegalMoves(true).Length 
-                + board.GetPieceList(PieceType.Queen, board.IsWhiteToMove).Count * pieceValues[0] 
-                + board.GetPieceList(PieceType.Rook, board.IsWhiteToMove).Count * pieceValues[1]
-                + board.GetPieceList(PieceType.Bishop, board.IsWhiteToMove).Count * pieceValues[2] 
-                + board.GetPieceList(PieceType.Knight, board.IsWhiteToMove).Count * pieceValues[3] 
-                + board.GetPieceList(PieceType.Pawn, board.IsWhiteToMove).Count * pieceValues[4];
-            board.UndoSkipTurn();
-            return c -board.GetLegalMoves().Length-board.GetLegalMoves(true).Length
-                 - board.GetPieceList(PieceType.Queen, board.IsWhiteToMove).Count * pieceValues[0]
-                - board.GetPieceList(PieceType.Rook, board.IsWhiteToMove).Count * pieceValues[1]
-                - board.GetPieceList(PieceType.Bishop, board.IsWhiteToMove).Count * pieceValues[2]
-                - board.GetPieceList(PieceType.Knight, board.IsWhiteToMove).Count * pieceValues[3]
-                - board.GetPieceList(PieceType.Pawn, board.IsWhiteToMove).Count * pieceValues[4];
-        }
-
-        if (depth == 3 && lm%2 == 1)
-        {
-            if (!board.TrySkipTurn())
-            {
-                return board.GetLegalMoves().Length;
-            }
-            c = board.GetLegalMoves().Length;
-            board.UndoSkipTurn();
-            return board.GetLegalMoves().Length - c;
-        }
-        depth++;
+        depth--;
 
         for (int i = 0; i < c; i++)
         {
             board.MakeMove(moves[i]);
 
-            score = RecursiveFunction(board, timer, depth);
-            if (score > high_score)
+            score = NegaMax(board, timer, depth);
+            if (score > max_score)
             {
-                high_score = score;
+                max_score = score;
             }
 
-            if (score < low_score)
-            {
-                low_score = score;
-            }
-         
             board.UndoMove(moves[i]);
         }
 
-        return (depth%2== 0) ? high_score:low_score;
+        return -max_score;
     }
 }
